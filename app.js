3352a0d5ef53066e928bb2accf7c6ff22fbbf064
rafbot-v7 <rafiqkhan8971@gmail.com>
2024-09-17 21:47:17 +0530

    resolved issues

--- app.js
+++ app.js
@@ -235,6 +235,7 @@ app.post('/addedu',(req,res)=>{
 app.post('/addpro', upload.single('proimage'), (req, res) => {
   const { pro, pro_desc,link, id } = req.body;
   const proimage = req.file ?  req.file.buffer : null;  
+  console.log( pro, pro_desc,link, id ,proimage)
 
   const insertqry = 'INSERT INTO protable(pro, pro_desc, user_id, img,link) VALUES(?,?,?,?,?)';
   const values = [pro, pro_desc, id, proimage,link];
@@ -311,9 +312,9 @@ app.post('/update', upload.fields([
   } else if (pro_id) {
        
       const { pro, pro_desc,link } = req.body;
-      const img = req.file ? req.file.buffer : null;  
+      const img = req.files && req.files['img'] ? req.files['img'][0].buffer : null; 
       console.log(img,req.file)
-      let query = `UPDATE protable SET pro = ?, pro_desc = ? link = ?`;
+      let query = `UPDATE protable SET pro = ?, pro_desc = ?, link = ?`;
       const params = [pro, pro_desc,link];
       if (img) {
           query += `, img = ?`;
@@ -329,7 +330,7 @@ app.post('/update', upload.fields([
   } else if (int_id) {
       
       const { intern, intern_desc } = req.body;
-      const img = req.file ? req.file.buffer : null;
+      const img = req.files && req.files['img'] ? req.files['img'][0].buffer : null; 
       console.log(req.file)
       let query = `UPDATE internshiptable SET intern = ?, int_desc = ?`;
       const params = [intern, intern_desc];
